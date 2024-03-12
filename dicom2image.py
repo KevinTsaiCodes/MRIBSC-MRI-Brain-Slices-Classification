b1479eae0c19efb64c84457ec667ed132bd6dfcd
KevinTsaiCodes <coachweichun@gmail.com>
2024-03-12 15:01:31 +0800

    update dicom2image.py

--- dicom2image.py
+++ dicom2image.py
@@ -9,10 +9,28 @@ import numpy as np
 
 import argparse
 import os
+import sys
 
 
 def dicom2image(input_directory: str, output_directory: str, brightness: float):
-    pass
+    for dicom in os.listdir(input_directory):
+        dicom_file_path = os.path.join(input_directory, dicom)  # create full filepath
+        if not dicom_file_path.endswith(".dcm"):
+            print(f"Skipping {dicom_file_path} because it doesn't with .dcm extension'")
+            continue
+        try:  # try the conversion
+            dicom_file = pydicom.dcmread(dicom_file_path)
+            if "VOILUTSequence" in dicom_file:
+                image_file = apply_voi_lut(dicom_file.pixel_array, dicom_file)
+
+            else:
+                image_file = dicom_file.pixel_array.astype(np.float32) * brightness
+
+        except pydicom.errors.InvalidDicomError as invalid_dicom:
+            invalid_dicom.println("Finished Skipping")
+
+        finally:
+            print("Finished conversion")
 
 
 if __name__ == "__main__":
